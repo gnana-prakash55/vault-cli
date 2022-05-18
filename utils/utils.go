443dcb871baa230c3ef2e063439050b441ed68d9
gnana-prakash55 <sanjayprakashsm@gmail.com>
2022-05-18 23:04:28 +0530

    last commit

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"archive/zip"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -12,9 +13,11 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-const URL = "http://localhost:3000/repo/upload"
+const URL = "http://localhost:3000/repo/put"
+const GET_URL = "http://localhost:3000/repo/get"
 
 type Token struct {
 	Token string `json:"token"`
@@ -134,4 +137,110 @@ func UploadFiles(path, token string) (string, error) {
 
 	return string(content), nil
 
+}
+
+func GetFiles(token string) {
+
+	config, err := ReadConfig()
+
+	body, err := json.Marshal(config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	request, err := http.NewRequest("POST", GET_URL, bytes.NewBuffer(body))
+
+	if err != nil {
+		panic(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Authorization", "Bearer "+token)
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Logging in...")
+
+	defer response.Body.Close()
+
+	content, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = Unzip(content, "content")
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Unzip(src []byte, dest string) error {
+	r, err := zip.NewReader(bytes.NewReader(src), int64(len(src)))
+	if err != nil {
+		return err
+	}
+
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip (Directory traversal)
+		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", path)
+		}
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
